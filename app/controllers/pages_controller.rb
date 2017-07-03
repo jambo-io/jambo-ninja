449182cca1f4f0d13136e7367ec6f0fc07ef3f57
Guilherme <guilhermewnunes@bitbucket.org>
2017-07-03 03:14:37 -0400

    About and Theme static pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,10 @@
  class PagesController < ApplicationController
     def show
-    	render template: "pages/#{params[:page]}"
+    	#render template: "pages/#{params[:page]}"
     end
-     def form
-  	 end
+    def themes
+    end
+    def about
+    end
+
   end  
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get "/page/:page" => "pages#show"
+  get ':permalink', :controller => 'pages', :action => 'about', :as => 'my_about'
+  get ':permalink', :controller => 'pages', :action => 'themes', :as => 'my_themes'
   get "/form" => "pages#form"
-  root "pages#index", page: "index"
+  root "pages#index", page: "index", :as => 'root'
+
+
 
 end
