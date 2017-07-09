85f42bd1d21bcc7f7ff8b409c58f307e6f0b64c8
Guilherme Nunes <guilhermewnunes@gmail.com>
2017-07-08 23:47:11 -0400

    Email text

--- app/mailers/form_conference_mailer.rb
+++ app/mailers/form_conference_mailer.rb
@@ -3,6 +3,6 @@ class FormConferenceMailer < ApplicationMailer
 
 	def conf_contact(user)
 		@user = user
-		mail(to:@user.email, subject:'Sample Email')
+		mail(to:@user.email, subject:'Conferências Locais de Juventude')
 	end
 end
