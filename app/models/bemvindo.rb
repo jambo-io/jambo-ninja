6a0b1ba566be90e137b0af7c06eddd918a12bca3
Guilherme Nunes <guilhermewnunes@gmail.com>
2017-08-25 07:25:58 -0400

    uniqueness

--- app/models/bemvindo.rb
+++ app/models/bemvindo.rb
@@ -2,7 +2,7 @@ class Bemvindo < ApplicationRecord
 	#validates :name, :email, :city, :presence => true
 	validates :name, :email, :city, :presence => true
 	
-	validates_uniqueness_of :email, :allow_blank => true, :allow_nil => true, :message => 'Esse email já foi utilizado, parece que você já está inscrito.'	
+	validates_uniqueness_of :email, if:'email.present?', :message => 'Esse email já foi utilizado, parece que você já está inscrito.'	
 	
 	
 	#validates :name, presence: {message: "Mensagem"}
