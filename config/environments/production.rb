509d1ace2479f2b6bba6d410799f558a660109c9
Guilherme Nunes <guilhermewnunes@gmail.com>
2018-08-31 21:19:12 -0400

    refresh

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,6 +89,21 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
+  config.assets.debug = true
+    #Amazon Storage
+  config.paperclip_defaults = {
+      storage: :s3,
+      s3_host_name: "s3-sa-east-1.amazonaws.com",
+      s3_region: ENV['AWS_REGION'],
+
+      s3_credentials: {
+          bucket: ENV['AWS_BUCKET'],
+          access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+          secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+      }
+
+  }
+
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
