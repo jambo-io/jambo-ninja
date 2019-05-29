39853962701b16a8218ab205c35d15937cbcdbdf
Guilherme <guilhermenunes09@protonmail.com>
2019-05-29 15:49:51 +0000

    Config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,7 +96,7 @@ Rails.application.configure do
     #Amazon Storage
   config.paperclip_defaults = {
       storage: :s3,
-      s3_host_name: "s3-sa-east-1.amazonaws.com",
+      s3_host_name: "jambo-ninja.s3.amazonaws.com",
       s3_region: ENV['AWS_REGION'],
 
       s3_credentials: {
