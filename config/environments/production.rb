bca9a5b0d4a5bf39e43deb8d16c64f5684a37594
Guilherme <guilhermenunes09@protonmail.com>
2019-05-29 15:56:29 +0000

    Config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,13 +96,14 @@ Rails.application.configure do
     #Amazon Storage
   config.paperclip_defaults = {
       storage: :s3,
-      s3_host_name: "jambo-ninja.s3.amazonaws.com",
+      s3_host_name: "s3-sa-east-1.amazonaws.com",
       s3_region: ENV['AWS_REGION'],
 
       s3_credentials: {
           bucket: ENV['AWS_BUCKET'],
           access_key_id: ENV['AWS_ACCESS_KEY_ID'],
           secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+          s3_region: ENV['AWS_REGION']
       }
 
   }
