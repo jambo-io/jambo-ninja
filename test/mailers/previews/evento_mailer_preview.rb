695ae3a22d396dad0de5d39714aafb2dd9214563
Guilherme <guilhermenunes09@protonmail.com>
2019-06-19 19:32:48 +0000

    New Mailer

--- test/mailers/previews/evento_mailer_preview.rb
+++ test/mailers/previews/evento_mailer_preview.rb
@@ -1,4 +1,9 @@
 # Preview all emails at http://localhost:3000/rails/mailers/evento_mailer
 class EventoMailerPreview < ActionMailer::Preview
 
+    def confirmation_email
+        @participant = Participant.last
+        EventoMailer.confirmation_email(@participant)
+	end
+
 end
